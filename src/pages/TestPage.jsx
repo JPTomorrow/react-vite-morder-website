51926af441aede4a46ff7fc6d67d110201a80b02
JPMorrow <justin.parker.morrow@gmail.com>
2022-03-17 06:14:39 -0500

    working with animation

--- src/pages/TestPage.jsx
+++ src/pages/TestPage.jsx
@@ -1,15 +1,36 @@
-import "@/pages/TestPage.css";
+import styles from "@/pages/TestPage.module.css";
+import { useState } from "react";
 import { motion } from "framer-motion";
 
-const body = (
-  <main style={{ padding: "1rem 0" }}>
-    <h2>TestPage</h2>
-    <motion.div animate={{ y: -5 }} transition={{ duration: 0.5 }}>
-      <p>Test Animation</p>
-    </motion.div>
-  </main>
-);
-
 export default function TestPage() {
-  return body;
+  const [count, setCount] = useState(0);
+  const [rotation, setRotation] = useState(45);
+
+  function increment() {
+    setCount(count + 1);
+    setRotation(rotation + 360);
+  }
+
+  return (
+    <main style={{ padding: "1rem 0" }}>
+      <h2>TestPage</h2>
+      <div id={styles["motion-outer"]}>
+        <motion.div
+          id={styles["inner-spin-square"]}
+          animate={{ rotate: rotation }}
+          transition={{ duration: 0.5 }}
+        />
+
+        <div id={styles["count"]}>
+          <p>{count}</p>
+        </div>
+      </div>
+
+      <div id={styles["btn-container"]}>
+        <button id={styles["increment-btn"]} onClick={() => increment()}>
+          increment
+        </button>
+      </div>
+    </main>
+  );
 }

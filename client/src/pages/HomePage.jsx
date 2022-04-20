5361349a9944bfb8f6e73c36b18868e9a5cd2335
Justin Morrow <justin.parker.morrow@gmail.com>
2022-04-20 16:06:32 -0500

    WaveText is mostly finished need particle bg

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,26 +1,7 @@
 import NewsEntry from "@/components/NewsEntry";
 import Button from "@/components/Button";
 import SearchBar from "@/components/SearchBar";
-import { motion } from "framer-motion";
-
 import newsEntries from "@/data/news-entries.json";
-
-const AnimatedText = ({ children }) => {
-  return (
-    <div className="flex">
-      {children.split("").map((letter, i) => {
-        return (
-          <motion.div
-            key={i}
-            className="inline-block text-white text-center text-5xl font-light"
-          >
-            {letter === " " ? "\u00A0" : letter}
-          </motion.div>
-        );
-      })}
-    </div>
-  );
-};
 
 function HomePage() {
   const updateSearch = (searchVal) => {
@@ -39,14 +20,35 @@ function HomePage() {
       {/* <SearchBar className={styles["search-bar"]} onChange={updateSearch} /> */}
       <div className="space-bg">
         <div className="homepage-banner">
-          <AnimatedText>Justin Morrow</AnimatedText>
-          <p className="text-white text-center text-xl font-light w-[50%] mt-5">
+          <WaveText
+            startDelay={0.5}
+            startHidden={true}
+            loop={false}
+            className="inline-block text-white text-center text-5xl font-light"
+          >
+            Justin Morrow
+          </WaveText>
+          <WaveText
+            startDelay={2.0}
+            startHidden={true}
+            className="text-white text-center text-xl font-light w-[50%] mt-5"
+          >
             {quote_lines[0]}
-            <br />
+          </WaveText>
+          <WaveText
+            startDelay={2.0}
+            startHidden={true}
+            className="text-white text-center text-xl font-light w-[50%]"
+          >
             {quote_lines[1]}
-            <br />
+          </WaveText>
+          <WaveText
+            startDelay={2.0}
+            startHidden={true}
+            className="text-white text-center text-xl font-light w-[50%]"
+          >
             {quote_lines[2]}
-          </p>
+          </WaveText>
         </div>
       </div>
       <div className="bg-gray-900 pb-10 pl-10">

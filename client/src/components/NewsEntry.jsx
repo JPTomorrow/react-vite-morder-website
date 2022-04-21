4bf392b441ed4cd1b286a8df54ea3d9ec06650e7
Justin Morrow <justin.parker.morrow@gmail.com>
2022-04-21 16:03:20 -0500

    working on blog

--- client/src/components/NewsEntry.jsx
+++ client/src/components/NewsEntry.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function NewsEntry(props, { className }) {
   return (
-    <div className="bg-transparent pt-5 pb-5">
-      <div className="default-header">{props.header}</div>
-      <div className="text-white">{props.content}</div>
+    <div className="news-entry">
+      <div className="default-header text-gray-700">{props.header}</div>
+      <div className="text-gray-700">{props.content}</div>
       {props.buttons}
     </div>
   );
--- client/src/pages/NewsPage.jsx
+++ client/src/pages/NewsPage.jsx
@@ -10,24 +10,26 @@ const updateSearch = (searchVal) => {
 
 function NewsPage() {
   return (
-    <div className="bg-gray-900 pb-10 pl-10 h-screen">
-      <h1 className="text-white text-3xl pb-5 pt-5">News</h1>
+    <div className="bg-gray-900 pb-10 h-screen">
+      <h1 className="text-white text-3xl pt-20 w-screen text-center">Blog</h1>
+      <div className="grid grid-cols-2 gap-5 m-10 grid-flow-row-dense">
+        {Object.values(newsEntries).map((entry, index) => {
+          return (
+            <NewsEntry
+              className="mt-5"
+              header={entry.name}
+              content={entry.content}
+              key={index}
+              buttons={
+                <Button onClick={() => console.log("clicked")}>
+                  {entry.button_text}
+                </Button>
+              }
+            />
+          );
+        })}
+      </div>
       {/* <SearchBar className={styles["search-bar"]} onChange={updateSearch} /> */}
-      {Object.values(newsEntries).map((entry, index) => {
-        return (
-          <NewsEntry
-            className="mt-5"
-            header={entry.name}
-            content={entry.content}
-            key={index}
-            buttons={
-              <Button onClick={() => console.log("clicked")}>
-                {entry.button_text}
-              </Button>
-            }
-          />
-        );
-      })}
     </div>
   );
 }
